8587b1d3ac634f5caa12209d602316b3caa70523
sevak <seva.kabrits@gmail.com>
2025-06-27 05:20:22 +0300

    fixed bug when restarted server not getting result with 404 by rearranging functions order; added custom scripts

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ SAFE_SCHEDULING = os.getenv("SAFE_SCHEDULING", "False").strip().lower() == "true
 
 token_path = os.path.join(os.getcwd(), "results.token")
 enso_file_path = os.path.join(os.getcwd(), ".ensorc")
+custom_script_file_path = os.path.join(os.getcwd(), "run.sh")
 
 PID = os.getpid()
 try:
@@ -26,13 +27,13 @@ POD_IP = socket.gethostbyname(socket.gethostname())
 OK = False
 
 
-def template_values(token, pod_ip, file_data, cpu_num, cpu_requests, mem, memory_requests, replica_num, aditional_params):
+def template_values(token, pod_ip, file_data, cpu_num, cpu_requests, mem, memory_requests, replica_num, aditional_params, custom_script_file_data):
     jinja2_template = jinja2.Environment(loader=jinja2.FileSystemLoader(SCRIPT_PATH)).get_template("values.tpl.yaml")
     values_data = jinja2_template.render(token=token, pod_ip=pod_ip, file=file_data,
                                          cpu_num=cpu_num, cpu_requests=cpu_requests, mem=mem, mem_requests=memory_requests,
                                          replica_num=replica_num, additional_params=aditional_params,
                                          shm=os.getenv("ORCA_SHARED_MEMORY_ENABLED", "true"),
-                                         shm_size=os.getenv("ORCA_SHARED_MEMORY_SIZE", "2Gi"))
+                                         shm_size=os.getenv("ORCA_SHARED_MEMORY_SIZE", "2Gi"), custom_script=custom_script_file_data)
 
     with open(os.path.join(SCRIPT_PATH, "values.yaml"), "w") as f:
         f.write(values_data)
@@ -99,6 +100,57 @@ if "--help" in sys.argv or "-h" in sys.argv:
     print("Use environment variable ORCA_SHARED_MEMORY_SIZE to set shared memory size if enabled,")
     print("                         possible values: XGi/XMi/XKi, where X - integer, default: 2Gi.\n")
     print("Use 'orca-executor exit' to finish orca gracefully.\n")
+    print("\nTo run custom calculations, e.g. enso, place your bash code in run.sh file.\n")
+    print("ORCA fullpath can be obtained with the command \"`echo $PATH | awk 'BEGIN{FS=\":\"; OFS=\"\\n\"} {$1=$1} 1' | grep orca | head -n 1`/orca\".\n")
+    print("\nFor running enso calculations place .ensorc file in the same directory with following config:\n")
+    print("""#NMR data
+reference for 1H: TMS                      # ('TMS',)
+reference for 13C: TMS                     # ('TMS',)
+reference for 19F: CFCl3                   # ('CFCl3',)
+reference for 31P: TMP                     # ('TMP', 'PH3')
+reference for 29Si: TMS                    # ('TMS',)
+1H active: on                              # ('on', 'off')
+13C active: off                            # ('on', 'off')
+19F active: off                            # ('on', 'off')
+31P active: off                            # ('on', 'off')
+29Si active: off                           # ('on', 'off')
+resonance frequency: None                  # MHz number of your experimental spectrometer
+nconf: all                                 # ('all', 'number e.g. 10')
+charge: 0                                  # number e.g. 0
+unpaired: 0                                # number e.g. 0
+solvent: gas                               # (acetone, acetonitrile, chcl3, ch2cl2, dmso, h2o, methanol, thf, toluene, gas)
+prog: None                                 # ('tm', 'orca')
+ancopt: on                                 # ('on', 'off')
+prog_rrho: xtb                             # ('xtb', 'prog', 'off')
+gfn_version: gfn2                          # ('gfn1', 'gfn2')
+temperature: 298.15                        # temperature in K e.g. 298.15
+prog3: prog                                # ('tm', 'orca', 'prog')
+prog4: prog                                # ('tm', 'orca', 'prog')
+part1: on                                  # ('on', 'off')
+part2: on                                  # ('on', 'off')
+part3: on                                  # ('on', 'off')
+part4: on                                  # ('on', 'off')
+boltzmann: off                             # ('on', 'off')
+backup: off                                # ('on', 'off')
+func: pbeh-3c                              # ('pbeh-3c', 'b97-3c', 'tpss')
+func3: pw6b95                              # ('pw6b95', 'wb97x', 'dsd-blyp')
+basis3: def2-TZVPP                         # (several basis sets are possible)
+funcJ: pbe0                                # ('tpss', 'pbe0')
+basisJ: def2-TZVP                          # (several basis sets are possible)
+funcS: pbe0                                # ('tpss', 'pbe0', 'dsd-blyp')
+basisS: def2-TZVP                          # (several basis sets are possible)
+couplings: on                              # ('on', 'off')
+shieldings: on                             # ('on', 'off')
+part1_threshold: 4.0                       # number e.g. 4.0
+part2_threshold: 2.0                       # number e.g. 2.0
+sm: default                                # ('cosmo', 'dcosmors', 'cpcm', 'smd')
+smgsolv2: sm                               # ('sm', 'cosmors', 'gbsa_gsolv')
+sm3: default                               # ('cosmors', 'smd', 'gbsa_gsolv')
+sm4: default                               # ('cosmo', 'cpcm', 'smd')
+check: on                                  # ('on', 'off')
+crestcheck: off                            # ('on', 'off')
+maxthreads: 0                              # number e.g. 2
+omp: 0                                     # number e.g. 4\n""")
     sys.exit(0)
 
 if sys.argv[1] == "exit":
@@ -156,6 +208,36 @@ if sys.argv[1] == "show":
 app = Flask(__name__)
 
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    auth = request.headers.get("X-Api-Key")
+    if app.config['AUTH_TOKEN'] != auth:
+        return Response("Wrong auth token", status=401)
+
+    with open(app.config['UPLOAD_FILE'], 'wb') as f:
+        chunk_size = 4096
+        while True:
+            chunk = request.stream.read(chunk_size)
+            if not chunk:
+                break
+            f.write(chunk)
+
+    with zipfile.ZipFile(app.config['UPLOAD_FILE'], 'r') as zip_ref:
+        zip_ref.extractall(os.path.dirname(app.config['UPLOAD_FILE']))
+
+    os.remove(app.config['UPLOAD_FILE'])
+
+    cleanup()
+
+    global OK
+    OK = True
+    pid = os.getpid()
+    assert pid == PID
+    os.kill(pid, signal.SIGINT)
+
+    return Response("File uploaded successfully", status=200)
+
+
 def run_app():
     app.run(host='0.0.0.0', port=8888)
     if not OK:
@@ -169,7 +251,7 @@ if os.path.exists(token_path):
     with open(token_path, 'r') as f:
         app.config['AUTH_TOKEN'] = f.read().replace("\n", "")
     print("Calculations already running! Waiting for the result.")
-    template_values(app.config['AUTH_TOKEN'], "127.0.0.1", "Hello world!", "2", "1", "2500Mi", "2000Mi", 1, "")
+    template_values(app.config['AUTH_TOKEN'], "127.0.0.1", "Hello world!", "2", "1", "2500Mi", "2000Mi", 1, "", '')
     run_app()
     sys.exit(0)
 else:
@@ -179,6 +261,12 @@ else:
 
 with open(os.path.join(os.getcwd(), sys.argv[1])) as f:
     file_data = f.read()
+
+if os.path.isfile(custom_script_file_path):
+    with open(custom_script_file_path) as f:
+        custom_script_file_data = f.read()
+else:
+    custom_script_file_data = ''
 
 cpus = get_cpu_data()
 req_cpus = get_available_cpu_data()
@@ -221,43 +309,13 @@ if int(mr[:-2]) > int(m[:-2]):
 if int(cr[:-1])/1000 > base_cpu+0.2:
     cr = base_cpu+0.2
 
-template_values(app.config['AUTH_TOKEN'], POD_IP, file_data, base_cpu + 0.2, cr, m, mr, replica_calculated, ((" " + " ".join(sys.argv[2:])) if len(sys.argv) > 2 else ""))
+template_values(app.config['AUTH_TOKEN'], POD_IP, file_data, base_cpu + 0.2, cr, m, mr, replica_calculated, ((" " + " ".join(sys.argv[2:])) if len(sys.argv) > 2 else ""), custom_script_file_data)
 
 res = subprocess.run(f"helm template orca-executor {os.path.join(SCRIPT_PATH, 'charts', 'openmpi-cluster')} {f'--set extraFiles[0].path=/opt/enso/config --set-file extraFiles[0].file={enso_file_path}' if os.path.isfile(enso_file_path) else ''} -f {os.path.join(SCRIPT_PATH, 'values.yaml')} | kubectl apply -f -", capture_output=True, shell=True)
 if res.returncode != 0:
     print("Failed to deploy orca-mpi cluster")
     print(res.stderr.decode("utf-8"))
     sys.exit(1)
-
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    auth = request.headers.get("X-Api-Key")
-    if app.config['AUTH_TOKEN'] != auth:
-        return Response("Wrong auth token", status=401)
-
-    with open(app.config['UPLOAD_FILE'], 'wb') as f:
-        chunk_size = 4096
-        while True:
-            chunk = request.stream.read(chunk_size)
-            if not chunk:
-                break
-            f.write(chunk)
-
-    with zipfile.ZipFile(app.config['UPLOAD_FILE'], 'r') as zip_ref:
-        zip_ref.extractall(os.path.dirname(app.config['UPLOAD_FILE']))
-
-    os.remove(app.config['UPLOAD_FILE'])
-
-    cleanup()
-
-    global OK
-    OK = True
-    pid = os.getpid()
-    assert pid == PID
-    os.kill(pid, signal.SIGINT)
-
-    return Response("File uploaded successfully", status=200)
 
 
 if __name__ == '__main__':
